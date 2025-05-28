0643143732101c9bfd5a929590d01ac59beb562c
Shweta Likhar <121114712+Shwetalikhar02@users.noreply.github.com>
2025-05-28 15:29:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -57,13 +57,39 @@ const updateStats = () => {
     const progress = (completedTasks / totalTasks) * 100;
     const progressBar = document.getElementById("progress");
 
-    progressBar.style.width = `$(progress)%`;
+    progressBar.style.width = `${progress}%`;
 
     document.getElementById("numbers").innerText = `${completedTasks} / ${totalTasks}`;
 
     if(tasks.length && completedTasks === totalTasks){
-        blaskConfetti();
-    }
+       const defaults = {
+            spread: 360,
+            ticks: 100,
+            gravity: 0,
+            decay: 0.94,
+            startVelocity: 30,
+            shapes: ["heart"],
+            colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+        };
+
+        confetti({
+            ...defaults,
+            particleCount: 50,
+            scalar: 2,
+        });
+
+        confetti({
+            ...defaults,
+            particleCount: 25,
+            scalar: 3,
+        });
+
+        confetti({
+            ...defaults,
+            particleCount: 10,
+            scalar: 4,
+        });
+    };
 };
 
 
